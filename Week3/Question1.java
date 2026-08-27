effc86a1939bbafdf60fe16d0e4e1bcc2b4a1e0e
JCode957 <jaisaketh2016@gmail.com>
2026-08-27 22:20:03 +0530

    Implement voting eligibility checker

---
+++ Week3/Question1.java
@@ -0,0 +1,14 @@
+public class Question1 {
+
+    static void checkVotingEligibility(int age) {
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkVotingEligibility(20);
+    }
+}
