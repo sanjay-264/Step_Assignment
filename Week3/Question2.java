c5968c64089508107a1ed2698499f910bbaf36ea
JCode957 <jaisaketh2016@gmail.com>
2026-08-27 22:20:20 +0530

    Implement number printer

---
+++ Week3/Question2.java
@@ -0,0 +1,12 @@
+public class Question2 {
+
+    static void printNumbersUpToN(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        printNumbersUpToN(5);
+    }
+}
