0628eea05ad47ef6ce644c1d47a0b462261775f3
JCode957 <jaisaketh2016@gmail.com>
2026-08-27 22:20:35 +0530

    Implement menu calculator

---
+++ Week3/Question4.java
@@ -0,0 +1,29 @@
+public class Question4 {
+
+    static void calculate(double a, double b, char operator) {
+        switch (operator) {
+            case '+':
+                System.out.println("Result: " + (a + b));
+                break;
+            case '-':
+                System.out.println("Result: " + (a - b));
+                break;
+            case '*':
+                System.out.println("Result: " + (a * b));
+                break;
+            case '/':
+                if (b == 0) {
+                    System.out.println("Cannot divide by zero");
+                } else {
+                    System.out.println("Result: " + (a / b));
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+
+    public static void main(String[] args) {
+        calculate(10, 4, '+');
+    }
+}
