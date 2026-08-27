db42a3d9b6771f64a99fb63747d719e6405d491a
JCode957 <jaisaketh2016@gmail.com>
2026-08-27 22:20:44 +0530

    Implement number pyramid printer

---
+++ Week3/Question5.java
@@ -0,0 +1,15 @@
+public class Question5 {
+
+    static void printNumberPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printNumberPyramid(4);
+    }
+}
