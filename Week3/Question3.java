b0b6febe7db5454afedbb01171de923cead0bc29
JCode957 <jaisaketh2016@gmail.com>
2026-08-27 22:20:27 +0530

    Implement multiple skipping printer

---
+++ Week3/Question3.java
@@ -0,0 +1,15 @@
+public class Question3 {
+
+    static void printSkippingMultiplesOfThree() {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 3 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        printSkippingMultiplesOfThree();
+    }
+}
