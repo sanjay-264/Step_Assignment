fddb93be2ddd748406e3602587f399daf12a7e2c
JCode957 <jaisaketh2016@gmail.com>
2026-09-04 10:59:56 +0530

    Implement subarray sum equals K

---
+++ Week4/Question4.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class Question4 {
+
+    static int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1};
+
+        System.out.println(subarraySum(nums, 2));
+    }
+}
