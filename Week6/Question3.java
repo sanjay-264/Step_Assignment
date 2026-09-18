7cd057f1f4b3072b809093e4dc013562c9e42e5f
JCode957 <jaisaketh2016@gmail.com>
2026-09-18 21:40:38 +0530

    Implement employee profile creation

---
+++ Week6/Question3.java
@@ -0,0 +1,39 @@
+package Week6;
+
+class EmployeeProfile {
+    String empId;
+    String empName;
+    double salary;
+    boolean isIntern;
+
+    public EmployeeProfile(String empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+        this.isIntern = false;
+    }
+
+    public EmployeeProfile(String empId, String empName) {
+        this(empId, empName, 0);
+        this.isIntern = true;
+    }
+
+    void printProfile() {
+        System.out.println(empId + " | " + empName + " | Rs "
+                + salary + " | Intern: " + isIntern);
+    }
+}
+
+public class Question3 {
+    public static void main(String[] args) {
+
+        EmployeeProfile employee =
+                new EmployeeProfile("E-101", "Divya", 65000);
+
+        EmployeeProfile intern =
+                new EmployeeProfile("E-102", "Arjun");
+
+        employee.printProfile();
+        intern.printProfile();
+    }
+}
